e043de07f267b07b508d4e6c987b43dbb2b274a5
Jason <wowthatisrandom@gmail.com>
2021-02-20 16:51:07 -0600

    hacks to fix main view - steve is a god

--- Sources/TeslaSwift/TeslaWebLoginViewContoller.swift
+++ Sources/TeslaSwift/TeslaWebLoginViewContoller.swift
@@ -29,12 +29,18 @@ public class TeslaWebLoginViewContoller: NSViewController {
 }
 
 extension TeslaWebLoginViewContoller: WKNavigationDelegate {
+    
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        let magic = "var interval = setInterval(Timer, 1000); var foundMFA = 0; function Timer() { try { var availableFactors = document.querySelector('#available-factors'); if (document.body.contains(availableFactors)) { if (foundMFA === 0) { foundMFA = 1; var mfaDiv = document.querySelector('#container > div'); if (document.body.contains(mfaDiv)) { mfaDiv.style.setProperty('position', 'fixed', 'important'); } } } } catch (err) { console.log(err.message); } try { var inputIdentity = document.querySelector('#form-input-identity'); if (document.body.contains(inputIdentity)) { inputIdentity.style.setProperty('position', 'fixed', 'important'); inputIdentity.style.maxWidth = '332px'; inputIdentity.style.margin = '0 auto 0 auto'; } } catch (err) { console.log(err.message); } try { var inputCredential = document.querySelector('#form-input-credential'); if (document.body.contains(inputCredential)) { inputCredential.style.setProperty('position', 'fixed', 'important'); inputCredential.style.maxWidth = '332px'; inputCredential.style.margin = '0 auto 0 auto'; } } catch (err) { console.log(err.message); } try { var inputPasscode = document.querySelector('#form-input-passcode'); if (document.body.contains(inputPasscode)) { if (foundMFA === 1) { foundMFA = 2; var mfaDivRev = document.querySelector('#container > div'); if (document.body.contains(mfaDivRev)) { mfaDivRev.style.removeProperty(position); } } inputPasscode.focus(); inputPasscode.style.setProperty('position', 'fixed', 'important'); inputPasscode.style.maxWidth = '332px'; inputPasscode.style.margin = '0 auto 0 auto'; } } catch (err) { console.log(err.message); }}"
+        
+            webView .evaluateJavaScript(magic, completionHandler: nil)
+    }
 
     public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
 
         if let url = navigationAction.request.url, url.absoluteString.starts(with: "https://auth.tesla.com/void/callback")  {
-            //AppDelegate.sharedInstance.applicationHandle(url: url)
             decisionHandler(.cancel)
+            
             let window:NSWindow = self.view.window!
             window.close()
             
